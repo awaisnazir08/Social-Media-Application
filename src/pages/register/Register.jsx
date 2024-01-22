5eb7e2360281c70a23cea723517c66f2723541f1
awaisnazir08 <owaisnazir2228@gmail.com>
2024-01-22 23:48:13 +0500

    login and register pages scss designed

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,10 +2,27 @@ import './register.scss'
 
 const Register = () => {
     return (
-        <div className='register'>
-
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <h1>Register</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <input type="text" placeholder="Name" />
+                        <button>Register</button>
+                    </form>
+                </div>
+                <div className="right">
+                    <h1>Little One</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita blanditiis, non accusantium molestias corrupti quo facilis rerum voluptatibus delectus doloribus.</p>
+                    <span>Do you have an account?</span>
+                    <button>Login</button>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Register
+export default Register;
